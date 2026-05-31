abf8591b9932c4630a8b58115b8353c3fb60dd13
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2026-05-31 19:42:39 +0200

    Post Card

---
+++ src/api/post.ts
@@ -0,0 +1,16 @@
+export interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+export const fetchPost = async(): Promise<Post[]> => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    if (!response.ok) {
+        throw new Error("Erreur API");
+    }
+
+    return response.json();
+}
