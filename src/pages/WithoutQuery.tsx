77140a7e027e8a411ee57c140c708a7fb37df385
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2026-06-01 09:31:56 +0200

    Without Query 3

--- src/pages/WithoutQuery.tsx
+++ src/pages/WithoutQuery.tsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
-import type { Post } from "../api/post";
+import { useEffect, useState } from "react";
+import { fetchPost, type Post } from "../api/post";
 
 export default function WithoutQuery() {
     const [posts, setPosts] = useState<Post[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
+
+    useEffect(() => {
+        const loadPosts = async () => {
+            try {
+                const data = await fetchPost();
+                setPosts(data);
+            } catch {
+                setError("Erreur de chargement");
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        loadPosts();
+    }, []);
 }
