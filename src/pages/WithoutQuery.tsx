7207f8909c1d44a92e9520c13dcd660b8c1f66e7
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2026-06-01 09:35:39 +0200

    Without Query 4

--- src/pages/WithoutQuery.tsx
+++ src/pages/WithoutQuery.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchPost, type Post } from "../api/post";
+import PostCard from "../components/PostCard";
 
 export default function WithoutQuery() {
     const [posts, setPosts] = useState<Post[]>([]);
@@ -20,4 +21,15 @@ export default function WithoutQuery() {
 
         loadPosts();
     }, []);
+
+    if (loading) return <p>Chargement ...</p>
+    if (error) return <p>{error}</p>
+
+    return (
+        <div className="grid gap-4">
+            {posts.slice(0, 10).map((post) => (
+                <PostCard key={post.id} post={post} />
+            ))}
+        </div>
+    );
 }
